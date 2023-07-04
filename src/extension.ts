aee904acc0af8b79aa561fa7bed80f4e8644d402
ormcat <64672841+ormcat@users.noreply.github.com>
2023-07-04 08:48:59 +0000

    feat ディレクトリ・ファイル作成

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,8 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
+import { fstat } from 'fs';
 import * as vscode from 'vscode';
+import { createInflate } from 'zlib';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -13,10 +15,18 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vscode-daily-note.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('vscode-daily-note.create-dailynote', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from vscode-daily-note!');
+		const workspaceUri = vscode.workspace.workspaceFolders?.[0].uri;
+		if (!workspaceUri) {
+			return vscode.window.showErrorMessage("ワークスペースがありません");
+		}
+ 		const baseDir = vscode.Uri.joinPath(workspaceUri, "articles");
+		vscode.workspace.fs.createDirectory(baseDir);
+
+		const todaysNote = vscode.Uri.joinPath(baseDir, "daily.md");
+		vscode.workspace.fs.writeFile(todaysNote, new TextEncoder().encode('# Y-m-d'));
 	});
 
 	context.subscriptions.push(disposable);
